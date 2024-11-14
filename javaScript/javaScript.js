f29da249aff41ee6749507194cb0b99923649435
ViacheslavDotsenko <nab.vyacheslav.dotsenko@gmail.com>
2024-11-14 18:13:21 +0200

    added

--- javaScript/javaScript.js
+++ javaScript/javaScript.js
@@ -1,9 +1,12 @@
 document.addEventListener("DOMContentLoaded", function() {
     const header = document.querySelector(".header");
+    const languageItems = document.querySelectorAll(".language_list");  
+    
     if (!header) {
         console.error("Елемент з класом 'header' не знайдено.");
         return; 
     }
+   
     window.addEventListener("scroll", function() {
         if (window.scrollY > 50) {         
             header.classList.add("fixed");
@@ -11,7 +14,15 @@ document.addEventListener("DOMContentLoaded", function() {
             header.classList.remove("fixed");
         }
     });
+   
+    languageItems.forEach(function(item) {
+        item.addEventListener("click", function() {           
+            if (item.classList.contains("selected")) return;
+           
+            languageItems.forEach(function(langItem) {
+                langItem.classList.remove("selected");
+            });            
+            item.classList.add("selected");
+        });
+    });
 });
-
-
-
