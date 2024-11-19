d1864da58a4a18c3de4d871b9e807145ee4afe9b
ViacheslavDotsenko <nab.vyacheslav.dotsenko@gmail.com>
2024-11-19 11:42:02 +0200

    added burger

--- javaScript/javaScript.js
+++ javaScript/javaScript.js
@@ -26,3 +26,20 @@ document.addEventListener("DOMContentLoaded", function() {
         });
     });
 });
+
+let header_burger = document.querySelector('.header_burger');
+let header_menu = document.querySelector('.header_menu');
+let back = document.querySelector('body');
+let header__list = document.querySelector('.header_list');
+
+header_burger.onclick = function(){
+    
+    header_burger.classList.toggle('active');
+    header_menu.classList.toggle('active');
+    back.classList.toggle('lock');
+};
+
+header__list.onclick = function () {
+    header__list.classList.remove('active');
+    back.classList.toggle('lock');
+}
